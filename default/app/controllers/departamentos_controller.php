3f791b843029081d8bc29f6ebd5da65fb969e0ec
TatianaDePaz <lisbethtati305@gmail.com>
2021-09-09 20:16:14 -0600

    modelo, controlador, index, create y edit de departamentos y municipios

---
+++ default/app/controllers/departamentos_controller.php
@@ -0,0 +1,62 @@
+<?php 
+Load::models('departamentos');
+
+class DepartamentosController extends AppController
+{
+    public function index($page=1) 
+    {
+        View::template('estilos_rutas');
+        $this->titulo = "Departamentos";
+        $departamento = new Departamentos();
+        $this->ListaDepartamentos = $departamento->getDepartamentos($page);
+    }
+
+    //create 
+
+    public function create()
+    {
+        View::template('estilos_rutas');
+        $this->titulo = "Departamentos";
+        if (input::hasPost('departamentos')) {
+            $departamento = new Departamentos(Input::post('departamentos'));
+            if ($departamento->save()) {
+                Flash::Valid("departamento agregado correctamente");
+                Input::delete();
+                return Redirect::to();
+            }
+            Flash::error("error al crear departamento");
+        }
+    }
+
+    //edit
+
+    public function edit($id)
+    {
+        View::template('estilos_rutas');
+        $this->titulo ="departamentos";
+        $Departamento = new Departamentos();
+        if (Input::hasPost('departamentos')) {
+
+            if(!$departamento->update(Input::post('departamentos'))){
+                Flash::error("error al editar departamentos");
+            } else {
+                Flash::valid("creado departamento exitosamente");
+                return Redirect::to();
+            }
+        }else{
+            $this->departamentos = $departamento->find((int)$id);
+        }
+    }
+
+    //delete
+
+    public function del($id){
+        $departamento = new Departamentos();
+        if(!$departamento->delete((int)$id)){
+            Flash::error("error al ingesar el departamento");
+        }else{
+            Flash::valid("ingresado con exito");
+        }
+        return Redirect::to();
+    }
+}
---
+++ default/app/controllers/municipios_controller.php
@@ -0,0 +1,62 @@
+<?php 
+Load::models('municipios');
+
+class MunicipiosController extends AppController
+{
+    public function index($page=1) 
+    {
+        View::template('estilos_rutas');
+        $this->titulo = "Municipios";
+        $municipio = new Municipios();
+        $this->listaMunicipios = $Municipio->getMunicipios($page);
+    }
+
+    //create 
+
+    public function create()
+    {
+        View::template('estilos_rutas');
+        $this->titulo = "municipios";
+        if (input::hasPost('municipios')) {
+            $municipio = new Municipios(Input::post('municipios'));
+            if ($municipio->save()) {
+                Flash::Valid("municipio agregado correctamente");
+                Input::delete();
+                return Redirect::to();
+            }
+            Flash::error("error al crear municipio");
+        }
+    }
+
+    //edit
+
+    public function edit($id)
+    {
+        View::template('estilos_rutas');
+        $this->titulo ="municipios";
+        $municipio = new Municipios();
+        if (Input::hasPost('municipios')) {
+
+            if(!$municipio->update(Input::post('municipios'))){
+                Flash::error("error al editar municipios");
+            } else {
+                Flash::valid("creado municipio exitosamente");
+                return Redirect::to();
+            }
+        }else{
+            $this->municipios = $municipio->find((int)$id);
+        }
+    }
+
+    //delete
+
+    public function del($id){
+        $municipio = new Municipios();
+        if(!$municipio->delete((int)$id)){
+            Flash::error("error al ingesar el municipio");
+        }else{
+            Flash::valid("ingresado con exito");
+        }
+        return Redirect::to();
+    }
+}
